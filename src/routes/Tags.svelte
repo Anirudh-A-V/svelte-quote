3be31aa27e3fb25857a65171ea9a2d336e01a444
Anirudh A V <anirudh.av02@gmail.com>
2023-01-28 00:36:23 +0530

    feat: tags enabled

--- src/routes/Tags.svelte
+++ src/routes/Tags.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 
     import { onMount } from 'svelte';
+    
+    import { selectedTag } from './store.js';
 
     let tags: any = [];
 
@@ -14,13 +16,22 @@
     onMount(() => {
         getTags();
     });
-</script>
+
+    const setTag = (e: any) => {
+        selectedTag.set(e.target.value)
+    };
+
+</script>   
 
 <div class="tags-body">
     <div class="tags">
-        {#each tags as tag}
-            <a href="/" class="tag">{tag}</a>
-        {/each}
+        <!-- Dropdown -->
+        <select class="dropdown">
+            <option value="all" selected on:click={setTag}>All</option>
+            {#each tags as tag}
+                <option value={tag} on:click={setTag}>{tag}</option>
+            {/each}
+        </select>
     </div>
 </div>
 
@@ -45,10 +56,30 @@
         padding: 1rem;
     }
 
-    .tag {
-        font-size: 1.5rem;
-        font-weight: 500;
-        margin-bottom: 1rem;
+    .dropdown {
+        width: 100%;
+        padding: 0.5rem;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        outline: none;
     }
+
+    .dropdown:focus {
+        border: 1px solid #000;
+    }
+
+    .dropdown option {
+        padding: 0.5rem;
+    }
+
+    .dropdown option:hover {
+        background-color: #ccc;
+    }
+
+    .dropdown option:active {
+        background-color: #000;
+        color: #fff;
+    }
+
 
 </style>
